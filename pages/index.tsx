def7f230e98ea676dabd5e460122b285850b3f16
Leonardo Lima <leonardo.lima@atip.io>
2022-10-15 17:09:21 -0300

    [feat]: struturas finais

--- pages/index.tsx
+++ pages/index.tsx
@@ -161,16 +161,16 @@ const Home: NextPage = () => {
             Países com maior <b>número de armas em posse de civis.</b>
           </p>
 
-          <p>
-            *Armas de fogo para cada 100 pessoas.
-          </p>
-
           <div className={styles.centralImage}>
+            <figcaption className={styles.caption}>
+              *Armas de fogo para cada 100 pessoas.
+            </figcaption>
+
             <figure>
               <Image
                 src="/images/posse-civil.png"
-                width={1000}
-                height={400}
+                width={900}
+                height={700}
                 alt="Gráfico número de armas em posse civil"
                 objectFit={'contain'}
               />
@@ -269,8 +269,8 @@ const Home: NextPage = () => {
             <figure>
               <Image
                 src="/images/violencia-armada.png"
-                width={1500}
-                height={700}
+                width={1000}
+                height={800}
                 alt="Gráfico de aumento da violência armada"
                 objectFit={'contain'}
               />
@@ -283,17 +283,98 @@ const Home: NextPage = () => {
             <strong>Tiroteios em escolas dos EUA atingiram um recorde em 2021</strong>
           </p>
 
-          <p>
-            *Vítimas por ano.
+          <div className={styles.centralImage}>
+            <figcaption>
+              *Vítimas por ano.
+            </figcaption>
+
+            <figure>
+              <Image
+                src="/images/vitimas-ano.png"
+                width={1500}
+                height={800}
+                alt="Gráfico de aumento de tiroteios em escolas dos EUA em 2021"
+                objectFit={'contain'}
+              />
+            </figure>
+          </div>
+        </section>
+
+        <section className={styles.defaultContainer}>
+          <p className={styles.defaultText}>
+            <strong>Em 10 anos EUA</strong> têm <strong>mais mortos em massacres</strong>
+            do que em <strong>ataques terroristas</strong>
           </p>
 
           <div className={styles.centralImage}>
             <figure>
               <Image
-                src="/images/vitimas-ano.png"
-                width={1500}
+                src="/images/terrorismo.png"
+                width={1000}
                 height={600}
-                alt="Gráfico de aumento de tiroteios em escolas dos EUA em 2021"
+                alt="Gráfico de quantidade de ataques terroristas"
+                objectFit={'contain'}
+              />
+            </figure>
+
+            <figure>
+              <Image
+                src="/images/massacres.png"
+                width={1000}
+                height={800}
+                alt="Gráfico qunatidade de ataques a tiros"
+                objectFit={'contain'}
+              />
+            </figure>
+          </div>
+        </section>
+
+        <section className={styles.defaultContainer}>
+          <p className={styles.defaultText}>
+            <strong>Japão</strong> tem um dos <strong>menores números</strong> relacionados
+            a <strong>violência</strong> com <strong>armas de fogo</strong> no <strong>mundo</strong>
+          </p>
+
+          <p className={styles.subTexts}>
+            Com leis de controle de armas de fogo, Japão tem uma das menores taxas relacionadas
+            a violência causada por uso de armas de fogo no mundo.
+          </p>
+
+          <p className={styles.subTexts}>
+            Apenas 0.6 pessoas a cada 100 possuem armas de fogo no Japão, comparados com 6.2
+            pessoas no Reino Unido e 88.8 pessoas nos Estados Unidos.
+          </p>
+
+          <div className={styles.centralImage}>
+            <figure>
+              <Image
+                src="/images/menor-quantidade-armas-mundo.png"
+                width={900}
+                height={1400}
+                alt="Gráfico japão tem a menor quantidade de armas no mundo"
+                objectFit={'contain'}
+              />
+            </figure>
+          </div>
+        </section>
+
+        <section className={styles.defaultContainer}>
+          <p className={styles.defaultText}>
+            Alguns <strong>dados</strong> sobre a <strong>violência no Brasil</strong>
+          </p>
+
+          <p className={styles.subTexts}>
+            Anuário Brasileiro de Segurança Pública de 2021 mostra a convivência do Brasileiro
+            com a violência e a dificuldade das autoridades em tentar controlá-la
+          </p>
+
+          <div className={styles.centralImage}>
+            <figure>
+              <Image
+                src="/images/violencia-brasil.png"
+                width={900}
+                height={1400}
+                alt="Gráfico japão tem a menor quantidade de armas no mundo"
                 objectFit={'contain'}
               />
             </figure>
